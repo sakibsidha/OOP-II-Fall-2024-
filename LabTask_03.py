4ae2608decd70d0692a84119211eb1a6cab14661
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-09-09 11:53:19 +0600

    Update LabTask_03.py

--- LabTask_03.py
+++ LabTask_03.py
@@ -108,4 +108,85 @@ x = set(x)
 a = a.union(x)
 print(a)
 
+# SET
+
+# a = {1, 3, 5, 8, 3, 7}
+# b = {0, False, 1, 5}
+
+# print(a, b)
+
+# len - type
+# print(len(a), type(a))
+
+# add - remove
+# a.add(10)
+# a.remove(8)
+# print(a)
+
+# union - intersection - difference
+# c = a | b
+# print("union:", c)
+#
+# c = a & b
+# print("intersection:", c)
+#
+# c = a - b
+# print("difference:", c)
+
+# loop
+# for i in a:
+#     if i == 5:
+#         break
+#     else:
+#         print(i, end = " ")
+# print()
+
+# join [2, 3, 4]
+# x = [2, 3, 4]
+# x = set(x)
+# a = a.union(x)
+# print(a)
+
+
+# DICTIONARY
+
+Employee = {
+    "name": "Tanjiro Kamado",
+    "age": 40,
+    "type":{"developer":["ios", "android"]},
+    "permament":True,
+    "salary": 30000,
+    100 : (1, 2, 3),
+    4.5 : {5, 6, True, 7, 1},
+}
+print(Employee)
+
+# 1
+print(len(Employee), type(Employee))
+
+# 2
+print(Employee["type"]["developer"])
+
+# 3
+Employee["permament"] = False
+
+# 4
+Employee["gender"] = "male"
+
+# 5
+del Employee["age"]
+
+# 6
+for i in Employee.keys():
+    print(i, end = " ")
+print()
+
+for i in Employee.values():
+    print(i, end = " ")
+print()
+
+for x, y in Employee.items():
+    print(x, y)
+
+
 
