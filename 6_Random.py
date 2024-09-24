b6565fd75260fecfde4e4da78c6b7dc4ecbce9b2
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-09-24 14:47:27 +0600

    Delete 6_Random.py

--- 6_Random.py
+++
@@ -1,7 +0,0 @@
-import random
-
-n = random.randint(0, 152)  
-
-print("Random Number between 0 to 152:", n)
-print("Random Number between 0 to 459:", random.randint(0, 459)) 
-print("Random Number between 56 to 98:", random.randint(56, 98))
