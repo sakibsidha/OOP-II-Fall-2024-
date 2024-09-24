f12013d643c84820efb06c6744682b4eb01357cb
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-09-24 14:44:30 +0600

    Delete 39_RandomString.py

--- 39_RandomString.py
+++
@@ -1,5 +0,0 @@
-import string    
-import random
-S = 7 # lenght
-ran = "".join(random.choices(string.ascii_letters + string.digits, k = S))    
-print("The randomly generated string is: " + str(ran))
