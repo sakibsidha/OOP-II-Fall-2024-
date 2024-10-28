764fd1abad5785032d5b2878ecf90c9acbe3ccb0
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-10-28 11:12:07 +0600

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -16,6 +16,42 @@ class Student(Person):
         super().display()
         print("Graduation Year:", self.graduation_year)
 
+class Teacher(Person):
+    def __init__(self, fname, lname, joining_year):
+        super().__init__(fname, lname)
+        self.joining_year = joining_year
+
+    def display(self):
+        super().display()
+        print("Joining Year:", self.joining_year)
+
+class Admin(Person):
+    def __init__(self, fname, lname, joining_year):
+        super().__init__(fname, lname)
+        self.joining_year = joining_year
+
+    def display(self):
+        super().display()
+        print("Joining Year:", self.joining_year)
+
+class CurrentStudent(Student):
+    def __init__(self, fname, lname):
+        super().__init__(fname, lname, 2026)
+        self.currentSemester = 5
+    
+    def display(self):
+        super().display()
+        print("Current Semester:", self.currentSemester)
+
+class Alumni(Student):
+    def __init__(self, fname, lname):
+        super().__init__(fname, lname, 6746)
+        self.passingYear = 1999
+    
+    def display(self):
+        super().display()
+        print("Passing Year:", self.passingYear)
+
 ob = Student("Tanjiro", "Kamado", 2026)
 ob.display()
 ob2 = Person("Nezuko", "Chan")
