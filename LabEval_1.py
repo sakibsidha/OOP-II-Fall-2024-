aee6858ecfae6586c5b93bbcb9d513d9d64e2508
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-12-02 11:06:13 +0600

    Create LabEval_1.py

---
+++ LabEval_1.py
@@ -0,0 +1,66 @@
+from abc import ABC, abstractmethod
+
+class BankAccount(ABC):
+    @abstractmethod
+    def deposit(self, amount):
+        pass
+
+    @abstractmethod
+    def withdraw(self, amount):
+        pass
+
+class InvalidAmountException(Exception):
+    pass
+
+class AccountHolder(BankAccount):
+    def __init__(self, name, balance, acc_num):
+        self._balance = balance
+        self.__name = name
+        self.__acc_num = acc_num
+    
+    def deposit(self, amount):
+        if (amount < 0):
+            raise InvalidAmountException
+        self._balance += amount
+        print("Deposit successful! New balance:", self._balance)
+    
+    def withdraw(self, amount):
+        if (amount < 0):
+            raise InvalidAmountException
+        if (amount > self._balance):
+            raise ValueError
+        self._balance -= amount
+        print("Withdraw successful! New balance:", self._balance)
+
+    def setBalance(self, balance):
+        self._balance = balance
+
+    def setName(self, name):
+        self.__name = name
+
+    def setAccountNum(self, acc_num):
+        self.__acc_num =  acc_num
+    
+    def getBalance(self):
+        return self._balance
+
+    def getName(self):
+        return  self.__name
+
+    def setAccountNum(self):
+        return self.__acc_num
+    
+a = AccountHolder("Raisa Haque", 100.0, 1234)
+try:
+    a.deposit(-1)
+    a.withdraw(103)
+except ValueError:
+    print("Value Error Occured!")
+except InvalidAmountException:
+    print("Invalid Amount Exception Occured.")
+except:
+    print("Other errors occured!")
+finally:
+    print("Done.")
+
+print(a.getName())
